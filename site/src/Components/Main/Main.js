2a66d93d07800a6eb96a2dda5068dcc9465a7fe5
rkopec91 <ryankopec91@gmail.com>
2020-09-16 14:33:16 -0400

    Update pointer on sidebar and toolbar.

--- site/src/Components/Main/Main.js
+++ site/src/Components/Main/Main.js
@@ -3,17 +3,14 @@ import './Main.css';
 import Resume from "./Resume/Resume";
 import Projects from './Projects/Projects';
 import About from './About/About';
-import Contact from './Contact/Contact';
 const Main = (props) => {
   let page = null;
   if (props.page[0]) {
     page = <About/>
   } else if (props.page[1]) {
     page = <Resume/>
-  } else if (props.page[2]) {
-    page = <Projects/>
   } else {
-    page = <Contact submit={props.handleSubmit}/>
+    page = <Projects/>
   }
 
   return (
