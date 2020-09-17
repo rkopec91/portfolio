29705c2d6062a6173e8b814132f4a0839309d2f6
rkopec91 <ryankopec91@gmail.com>
2020-09-16 20:50:14 -0400

    Update file structure.

---
+++ src/Components/Main/Projects/Projects.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import Thumbnail from './Thumbnail/Thumbnail';
+import '../../../App.css';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+
+function Projects(props) {
+  return (
+    // Render a Thumbnail component
+    <div>
+      <h1>Projects</h1>
+      <div className="projects">
+        <Container>
+          <Row>
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+            <Thumbnail
+              link="https://github.com/rkopec91/Behavioral_Cloning"
+              image="logo192.png"
+              title="Behavior Cloning"
+              category="Deep Learning"
+            />
+          </Row>
+            
+        </Container>
+        
+        
+      </div>
+    </div>
+  )
+}
+ 
+export default Projects;
---
+++ src/Components/Toolbar/Toolbar.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+import DrawerToggleButton from '../DrawerButton/DrawerButton'
+import './Toolbar.css';
+
+const toolbar = props => (
+    <header className="toolbar">
+        <nav className="toolbar__navigation">
+            <div className="toolbar__toggle-button">
+                <DrawerToggleButton click={props.drawerClickHandler} />
+            </div>
+            <div className="toolbar__logo">
+                <a onClick={() => props.pageClickHandler([true, false, false, false])}>RYAN KOPEC</a>
+            </div>
+            <div className="spacer" />
+            <div className="toolbar__navigation-items">
+                <ul>
+                    <li>
+                        <a onClick={() => props.pageClickHandler([true, false, false, false])} className="anchor">About</a>
+                    </li>
+                    <li>
+                        <a onClick={() => props.pageClickHandler([false, true, false, false])} className="anchor">Resume</a>
+                    </li>
+                    <li>
+                        <a onClick={() => props.pageClickHandler([false, false, true, false])} className="anchor">Projects</a>
+                    </li>
+                    <li>
+                        <a href="mailto:ryankopec91@gmail.com" className="anchor">Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+)
+
+export default toolbar
