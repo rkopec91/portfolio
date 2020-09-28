a4bec437d634ffdefe8a40c4356fc0b3acf6df62
rkopec91 <ryankopec91@gmail.com>
2020-09-28 15:29:38 -0400

    Update projects and main components

--- src/Components/Main/Projects/Projects.js
+++ src/Components/Main/Projects/Projects.js
@@ -5,6 +5,7 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import '../../../style/Projects.css';
 
 function Projects(props) {
   return (
@@ -12,94 +13,41 @@ function Projects(props) {
     <div>
       <h1>Projects</h1>
       <div>
-        {/* <Container>
-          <Row>
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-            <Thumbnail
-              link="https://github.com/rkopec91/Behavioral_Cloning"
-              image="logo192.png"
-              title="Behavior Cloning"
-              category="Deep Learning"
-            />
-          </Row>
-            
-        </Container> */}
         
         <Carousel>
           <Carousel.Item>
             <img
-              className="d-block w-100"
+              // className="d-block w-100"
               src="logo192.png"
               alt="First slide"
             />
             <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+              <h3 className="text">First slide label</h3>
+              <p className="text">Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
+              // className="d-block w-100"
               src="logo192.png"
               alt="Third slide"
             />
 
             <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+              <h3 className="text">Second slide label</h3>
+              <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
+              // className="d-block w-100"
               src="logo192.png"
               alt="Third slide"
             />
 
             <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+              <h3 className="text">Third slide label</h3>
+              <p className="text">Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
             </Carousel.Caption>
           </Carousel.Item>
           
