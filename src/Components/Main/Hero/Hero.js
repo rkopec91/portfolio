a8bc08466033b237afe792a07835368f5ac88f29
rkopec91 <ryankopec91@gmail.com>
2020-09-30 16:00:56 -0400

    Update hero and change picture

--- src/Components/Main/Hero/Hero.js
+++ src/Components/Main/Hero/Hero.js
@@ -7,19 +7,14 @@ const Hero = (props) => {
         <Jumbotron className="bg-transparent jumbotron-fluid p-0">
             <Container fluid={true}>
                 <Row className="justify-content-center py-5">
-                    <Col>
-                        <img src='44616.jpg' className="port-image" alt="The profile pic"/>
-                    </Col>
-                    <Col md={8} sm={12} style={{position: 'relative'}}>
+                        
                         <div>
-                            <p><a href="mailto:ryankopec91@gmail.com">RyanKopec@gmail.com</a></p>
-                            <p>(856) 749-9693</p>
-                        </div>
-                        <div style={{position: 'absolute', bottom:0}}>
+                        <img src='GOPRO.png' className="port-image" alt="The profile pic"/>
+                        
                             { props.title && <h1 className="display-1 font-weight-bolder">{props.title}</h1> }
                         </div>
                         
-                    </Col>
+                    
                 </Row>
             </Container>
         </Jumbotron>
