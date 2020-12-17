5cbb442b58f169431e08e87b5b593215771d4468
rkopec91 <ryankopec91@gmail.com>
2020-12-17 11:38:42 -0500

    Revamp Portfolio

--- src/Components/Backdrop/Backdrop.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import '../../style/Backdrop.css';
-
-const backrop = props => (
-    <div className="backdrop" onClick={props.click} />
-);
-
-export default backrop;
---
+++ src/constants/socialLinks.js
@@ -0,0 +1,42 @@
+import React from "react"
+import {
+  FaFacebookSquare,
+  FaLinkedin,
+  FaDribbbleSquare,
+  FaBehanceSquare,
+  FaTwitterSquare,
+  FaGithub
+} from "react-icons/fa"
+
+const data = [
+  {
+    id: 1,
+    icon: <FaFacebookSquare className="social-icon"></FaFacebookSquare>,
+    url: "https://www.facebook.com/ryan.kopec",
+  },
+  {
+    id: 2,
+    icon: <FaLinkedin className="social-icon"></FaLinkedin>,
+    url: "https://www.linkedin.com/in/ryan-kopec/",
+  },
+  {
+    id: 3,
+    icon: <FaGithub className="social-icon"></FaGithub>,
+    url: "https://github.com/rkopec91",
+  },
+]
+const links = data.map(link => {
+  return (
+    <li key={link.id}>
+      <a href={link.url} className="social-link">
+        {link.icon}
+      </a>
+    </li>
+  )
+})
+
+export default ({ styleClass }) => {
+  return (
+    <ul className={`social-links ${styleClass ? styleClass : ""}`}>{links}</ul>
+  )
+}
