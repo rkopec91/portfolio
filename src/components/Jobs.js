8793cab33df731524a1f2b03a20f83598296c6f4
rkopec91 <ryankopec91@gmail.com>
2020-12-18 15:48:37 -0500

    Add experience section

--- src/components/Jobs.js
+++ src/components/Jobs.js
@@ -3,9 +3,40 @@ import Title from "./Title"
 import { FaAngleDoubleRight } from "react-icons/fa"
 import { graphql, useStaticQuery } from "gatsby"
 import { Link } from "gatsby"
+import positions from "../constants/positions"
 
 const Jobs = () => {
-  return <h2>jobs component</h2>
+  console.log(positions)
+  const [value, setValue] = React.useState(0)
+  const { company, position, date, desc } = positions[value]
+
+  return <section className="section jobs">
+    <Title title="Experience"/>
+    <div className="jobs-center">
+      <div className="btn-container">
+        {positions.map((pos,index)=>{
+          return <button key={pos.id} onClick={()=>setValue(index)} className={`job-btn
+          ${index === value && 'active-btn'}`}>{pos.company}</button>
+        })}
+      </div>
+      <article className="job-info">
+        <h3>{position}</h3>
+        <h4>{company}</h4>
+        <p className="job-date">{date}</p>
+        {
+          desc.map((item) => {
+            return <div key={item.id} className="job-desc">
+              <FaAngleDoubleRight className="job-icon"></FaAngleDoubleRight>
+              <p>{item.name}</p>
+            </div>
+          })
+        }
+      </article>
+    </div>
+    <Link to="/about" className="btn center-btn">
+      more info
+    </Link>
+  </section>
 }
 
 export default Jobs
---
+++ src/constants/positions.js
@@ -0,0 +1,88 @@
+import React from "react"
+import { FaCode, FaSketch, FaAndroid, FaRobot, FaEye } from "react-icons/fa"
+export default [
+  {
+    id: 1,
+    company: "CVision AI",
+    position: "Senior Algorithm Developer",
+    date: "January 2020 - April 2020",
+    desc: [
+        {
+            id: 1,
+            name: "Improved Deep Learning Computer Vision algorithms in Keras/Tensorflow and developed analysis tools and scripts to view performance of algorithms."
+        },
+        {
+            id: 2,
+            name: "Trained Resnet and Retinanet architectures to detect and classify different marine species in videos."
+        },
+        {
+            id: 3,
+            name: "Developed tests in Jest and improved front-end written in Javascript, CSS and HTML."
+        },
+        {
+            id: 4,
+            name: "Maintained software using Python, Javascript, Docker and Kubernetes."
+        },
+    ]
+  },
+  {
+    id: 2,
+    company: "Georgia Tech",
+    position: "Graduate Teaching Assistant",
+    date: "September 2018 - December 2019",
+    desc: [
+        {
+            id: 1,
+            name: "Knowledge-Base AI"
+        },
+    ]
+  },
+  {
+    id: 3,
+    company: "Slyce",
+    position: "Software Engineer",
+    date: "April 2019 - August 2019",
+    desc: [
+        {
+            id: 1,
+            name: "Developed visual search applications using Python, Javascript, Angular, HTML, CSS MongoDB, PostgreSQL, Docker, TensorFlow, Keras, and Kubernetes."
+        },
+        {
+            id: 2,
+            name: "Built tools to analyze and increase the accuracy of developed machine learning and computer vision algorithms."
+        },
+        {
+            id: 3,
+            name: "Utilized Git and GitHub to update and track code."
+        },
+        {
+            id: 4,
+            name: "Maintained and updated web applications on Google Cloud Platform."
+        },
+    ]
+  },
+  {
+    id: 4,
+    company: "Lockheed Martin",
+    position: "Software Engineer",
+    date: "March 2015 - April 2019",
+    desc: [
+        {
+            id: 1,
+            name: "Debugged and developed software in C++ to improve the currently implemented system."
+        },
+        {
+            id: 2,
+            name: "Developed tools for analysis using Python, Pandas, NumPy, and MATLAB."
+        },
+        {
+            id: 3,
+            name: "Wrote Python, Pandas, and NumPy code to quickly process data received from the models and displayed in charts and graphs on a PowerPoint or PDF."
+        },
+        {
+            id: 4,
+            name: "Collaborated with cross-functional engineering teams to execute new filters and algorithms with C++, Git, and Jenkins to improve the system’s performance."
+        },
+    ]
+  },
+]
