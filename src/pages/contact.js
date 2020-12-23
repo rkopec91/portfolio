2829809a9037cfc410a252bedccd5bc94bb6e248
rkopec91 <ryankopec91@gmail.com>
2020-12-23 00:14:55 -0500

    Complete forms

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -2,7 +2,21 @@ import React from "react"
 import Layout from "../components/Layout"
 
 const contact = () => {
-  return <h2>contact page</h2>
+  return <Layout>
+    <section className="contact-page">
+      <article className="contact-form">
+        <h3>get in touch</h3>
+        <form name="contact" method="POST" data-netlify="true">
+          <div className="form-group">
+            <input type="text" name="name" placeholder="name" className="form-control"/>
+            <input type="email" placeholder="email" name="email" className="form-control"/>
+            <textarea name="message" rows="7" placeholder="message" className="form-control"></textarea>
+          </div>
+          <button type="submit" className="submit-btn btn">submit</button>
+        </form>
+      </article>
+    </section>
+  </Layout>
 }
 
 export default contact
